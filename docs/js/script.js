f0151843608ddf5c85364bbdae5ba9baae611c80
Med Aziz Azzabi <azizazzabi456@gmail.com>
2022-02-16 17:22:17 +0100

    Slider update

---
+++ docs/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('#responsive').lightSlider({
+        item:5,
+        loop:false,
+        slideMove:1,
+        slideMargin:20,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        responsive : [
+            {
+                breakpoint:992,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                  }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            },
+            {
+                breakpoint:576,
+                settings: {
+                    item:1,
+                    slideMove:1,
+                  }
+            },
+        ]
+    });  
+  });
